7285a80cb21905f32e15787966608b95c63efd1a
KevinJMLeyva <145345829+KevinJMLeyva@users.noreply.github.com>
2023-11-28 23:53:08 -0600

    Add files via upload

---
+++ 1_Funciones_Iterativas_Recursivas/funciones.h
@@ -0,0 +1,42 @@
+/*
+ * recursion.h
+ *
+ *  24/08/2023
+ *      Author:  Kevin J. Martínez Leyva
+ */
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+#include <iostream>
+
+class Funciones {
+public:
+	int sumaIterativa(int n){
+        if (n<=0)
+        {
+           return 0;
+        }
+        int resultado=0;
+        for(int i=1;i<=n;i++){
+            resultado=resultado+i;
+        }
+        return resultado;
+     };
+    int sumaRecursiva(int n){
+        if (n <= 0) {
+		return 0;
+	    } 
+        else if (n <= 1) {
+		return 1;
+	    }   
+        else {
+		    return n + sumaRecursiva(n - 1);
+	    }
+     };
+    int sumaDirecta(int n){
+        return n*(n+1)/2;
+    };
+};
+#endif 
+
